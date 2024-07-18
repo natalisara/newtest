d2935e22ccc88605f2d21c33492c46ba51b0455d
Natalia Saralidze <saralidze.natalia@gmail.com>
2024-07-18 16:36:10 +0400

    Change1

--- work.py
+++ work.py
@@ -1,1 +1,8 @@
 print("test")
+
+def getBirthdays(cYear, bYear, bMonth, bDay):
+    weekdayIndex = calendar.weekday(cYear, bMonth, bDay)
+    dayName = calendar.day_name[weekdayIndex]
+    age = cYear - bYear
+
+    return age, dayName
